2e6f299f19afa941959bfe96365125189589079b
dhainiksuthar <69338311+dhainiksuthar@users.noreply.github.com>
2020-12-04 14:54:15 +0530

    Update About.js

--- frontend/src/components/developers/About.js
+++ frontend/src/components/developers/About.js
@@ -11,7 +11,7 @@ const About = () => {
 
         <img src={png1} alt="Card image cap" />
 
-        <div class="card-body">
+        <div class="card-body new">
             <p class="card-text color"><b>Ankur A Zalavadiya</b></p>
             <a href="https://www.facebook.com/ankur.zalavadiya.3"><i class="fa fa-facebook-square icon color" aria-hidden="true"></i> </a>
             <a href="https://instagram.com/mr__perfect__1903?igshid=10ocwyv85t6ju"><i class="fa fa-instagram icon color ml-3 mr-3" aria-hidden="true"></i> </a>
@@ -21,7 +21,7 @@ const About = () => {
         <div class="card usd" >
        
         <img src={png2} alt="Card image cap" />
-        <div class="card-body">
+        <div class="card-body new">
             <p class="card-text color strong"><b>Ankit J Chotaliya</b></p>
              <a href="https://www.facebook.com/ankit.chotaliya.3"><i class="fa fa-facebook-square icon color" aria-hidden="true"></i></a>
              <a href="https://instagram.com/mr__perfect__3110?igshid=10ocwyv85t6ju"><i class="fa fa-instagram icon color ml-3 mr-3" aria-hidden="true"></i></a>
@@ -32,7 +32,7 @@ const About = () => {
         
         <img src={png3} alt="Card image cap" />
        
-        <div class="card-body">
+        <div class="card-body new">
             <p class="card-text color"><b>Dhainik A Suthar</b></p>
              <a href="https://www.facebook.com/dhainik.suthar"><i class="fa fa-facebook-square icon color" aria-hidden="true"></i> </a>
              <a href="https://instagram.com/dhainik_suthar?igshid=10ocwyv85t6ju"><i class="fa fa-instagram icon color ml-3 mr-3" aria-hidden="true"></i></a>
